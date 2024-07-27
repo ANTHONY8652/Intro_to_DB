3250079642eb91ef51f8d8b3a5914a9300107e01
ANTHONY8652 <githinjianthony720@gmail.com>
2024-07-27 22:04:28 +0300

    #RutoMustGo

--- task_6.sql
+++ task_6.sql
@@ -2,6 +2,6 @@ USE alx_book_store;
 -- bonyeza bonyeza ushindeee
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '125 Happiness Ave.'),
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
     (3, 'Obed Ehonoah', 'eobed@sandtech.com', '125 Happiness Ave.'),
     (4, 'Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
