dbd1d5acbd9b0ef8e5df48050a306e643a8ddba5
ANTHONY8652 <githinjianthony720@gmail.com>
2024-07-27 21:49:28 +0300

    anguka mzeeeee

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,6 @@
 USE alx_book_store;
 --bonyeza bonyeza ndani yake
-INSERT INTO customer(customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '125 Happiness Ave.'),
     (3, 'Obed Ehonoah', 'eobed@sandtech.com', '125 Happiness Ave.'),
