ddd3660df8fd13fef0111ad02a6d0560cc966d54
ANTHONY8652 <githinjianthony720@gmail.com>
2024-07-27 21:00:08 +0300

    Anguka nayo pia wewe sasa

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS Orders(
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 -- create meza ya kenye wameitisha
 );
-CREATE TABLE IF NOT EXISTS Order_details(
+CREATE TABLE IF NOT EXISTS Order_Details(
     order_details INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
