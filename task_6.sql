b7dd5da388afe95f2823a1cd5b69674b59421125
ANTHONY8652 <githinjianthony720@gmail.com>
2024-07-27 22:08:31 +0300

    #RutoMustGo

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,23 @@
-USE alx_book_store;
+"""USE alx_book_store;
+-- bonyeza bonyeza ushindeee
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (3, 'Obed Ehonoah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    (4, 'Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');"""
+
+"""USE alx_book_store;
 
 INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
     (3, 'Obed Ehonoah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+    (4, 'Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');"""
+
+USE alx_book_store;
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
