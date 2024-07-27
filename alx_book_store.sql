b1d2ae75222f2b57a58370304fd28c2251037054
ANTHONY8652 <githinjianthony720@gmail.com>
2024-07-27 20:42:01 +0300

    Anguka nayo

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -7,34 +7,34 @@ CREATE TABLE Authors(
     author_name VARCHAR(215) NOT NULL
 );
 -- create meza ya vitabu hehe
-CREATE TABLE Books(
+CREATE TABLE IF NOT EXISTS Books(
     book_id INT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
     price DOUBLE NOT NULL,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCING Authors(author_id) 
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id) 
 );
 -- create customers meza
 CREATE TABLE IF NOT EXISTS Customers(
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) UNIQUE NOT NULL,
-    address TEXT
+    address TEXT NOT NULL
 );
 -- create orders meza
-CREATE TABLE Orders(
+CREATE TABLE IF NOT EXISTS Orders(
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCING customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 -- create meza ya kenye wameitisha
 );
-CREATE TABLE Order_details(
+CREATE TABLE IF NOT EXISTS Order_details(
     order_details INT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity INT DOUBLE,
-    FOREIGN KEY(order_id) REFERENCING Orders(order_id),
-    FOREIGN KEY(book_id) REFERENCING Books(book_id),
+    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY(book_id) REFERENCES Books(book_id),
 );
